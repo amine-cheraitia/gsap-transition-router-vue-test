c84913199c8055308a51a575ae16bad8c163657c
Amine Cheraitia <amine-cheraitia@hotmail.com>
2023-03-10 19:42:19 +0100

    installation de gsap3 et son importation sur main js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { gsap } from "gsap";
+createApp(App).use(router).use(gsap).mount("#app");
